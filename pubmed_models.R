f38ce9766c8e86ba0b10416bdda1ef6ec70956c7
huangchv <vince.ch.huang@gmail.com>
2017-09-10 23:50:08 -0400

    tweaking hyperparameters and fitting model

--- pubmed_models.R
+++ pubmed_models.R
@@ -16,18 +16,26 @@ library(reshape2)
 library(caret)
 library(mlr)
 library(mxnet)
+options(scipen=999)
 
 
 ###### Data ########
 
 all.data <- readRDS('G://Analyses/pubmed/data/2017-09-09_pubmed_processed_data.rda')
 
+# Add ceiling to n_authors and center
+summary(all.data$n_author)
+authors_upper <- quantile(all.data$n_author, 0.99)
+temp.n <- all.data$n_author
+temp.n[temp.n>authors_upper] <- authors_upper
+
+all.data$n_authors_scaled <- scale(temp.n, center = TRUE, scale = TRUE)
 ########## regression ##########
-omit.cols <- c(1,2)
+omit.cols <- c(1,2,4,5,6)
 #train.data <- all.data[,-omit.cols]
 
 # Split data 
-data.part <- createDataPartition(all.data$year, p = 0.75, list=FALSE)
+data.part <- createDataPartition(all.data$citations, p = 0.75, list=FALSE)
 train.data <- all.data[data.part, -omit.cols]
 test.data <- all.data[-data.part, -omit.cols]
 
@@ -44,20 +52,16 @@ xgboost.lrn <- setHyperPars(
   eta = .03333, 
   subsample = .7, 
   max_depth=4, 
-  nrounds=250,
+  nrounds=300,
   eval_metric = 'rmse', 
   nthread=4, 
   gamma=1)
 
 
-# Tune hyperparameters
+# Tune nrounds
 rounds.params <- makeParamSet(
-  makeDiscreteParam('nrounds', values = seq(25,350,25))
-  #makeIntegerParam("max_depth",lower = 7L,upper = 10L),
-  #makeNumericParam("subsample",lower = 0.5,upper = 1), 
-  #makeNumericParam("colsample_bytree",lower = 0.5,upper = 1)
-)
-
+  makeDiscreteParam('nrounds', values = seq(25,650,25))
+  )
 
 # CV strat
 re.strat <- makeResampleDesc("CV", iters=4)
@@ -77,20 +81,26 @@ mytune <- tuneParams(
   control = ctrl, 
   show.info = T)
 
-
+# nrounds25? RMSE 19.6
 xgboost.model <- mlr::train(learner = xgboost.lrn, task = regression.task)
 
 test.pred <- predict(xgboost.model, newdata = test.data)
-#test.pred <- predict(xgboost.model, task = regression.task)
+train.pred <- predict(xgboost.model, task = regression.task)
+plot(test.pred$data$truth, test.pred$data$response)
+plot(train.pred$data$truth, train.pred$data$response)
+
+cor(test.pred$data$truth, test.pred$data$response, method='spearman')
+
 
 boop<-getFeatureImportance(xgboost.model)
 feature.iwant <- boop$res
 feature.iwant <- as.data.frame(t(feature.iwant))
 feature.iwant$feature <- rownames(feature.iwant)
-feature.iwant <- feautre.iwant[order(feature.iwant$V1, decreasing=TRUE),]
+feature.iwant <- feature.iwant[order(feature.iwant$V1, decreasing=TRUE),]
 head(feature.iwant, 25)
 
 plot(test.pred$data$truth, test.pred$data$response)
+plot(train.pred$data$truth, train.pred$data$response)
 
 cor(test.pred$data$truth, test.pred$data$response)
 plot(density(train.data$citations))
