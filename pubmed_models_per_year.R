489ecfaa96f0654b356307f4720872b325643af4
huangchv <vince.ch.huang@gmail.com>
2017-09-17 15:19:02 -0400

    WIP - plot of important features over time

--- pubmed_models_per_year.R
+++ pubmed_models_per_year.R
@@ -11,6 +11,7 @@ library(ggplot2)
 library(reshape2)
 library(caret)
 library(mlr)
+library(BoutrosLab.plotting.general)
 options(scipen=999)
 DATE <-format.Date(Sys.Date(),format = "%Y-%m-%d")
 
@@ -39,7 +40,10 @@ all.features.year <- lapply(target.years, function(x) {
 })
 
 names(all.features.year) <- paste0('y', target.years)
-temp <- all.features.year
+#
+#saveRDS(all.features.year, '2017-09-17_all_feature_importance.rda')
+all.features.year <- readRDS('2017-09-17_all_feature_importance.rda')
+
 #### Pull feature ranks per year ####
 # All year to everything in list
 for (i in 1:length(target.years)) {
@@ -65,8 +69,55 @@ features.df <- do.call(rbind, all.features.year)
 best.features.df <- features.df %>% 
   filter(feature %in% best.features)
 
+
+# Convert to factors
+best.features.df$feature <- as.factor(best.features.df$feature)
+feature.order <- levels(best.features.df$feature)
+
 best.features.matrix <- dcast(best.features.df, formula = feature ~ year, value.var = 'rank')
 
 best.features.matrix[order(best.features.matrix$`2010`),]
-# Plot changes over time 
 
+# To do
+# Set floor to 20, anything beyond that goes to 20
+# Relabel yaxis
+best.features.df$rank[best.features.df$rank > 15] <-15
+
+# Plot changes over time 
+create.scatterplot(
+  data = best.features.df,
+  formula = rank ~ year,
+  groups = feature,
+  col = default.colours(12),
+  xlimits = c(2009.8, 2015.2),
+  xat = 2010:2016,
+  type = 'b',
+  cex = 1.25,
+  ylimits = c(15,0),
+  yat = seq(0,15,3),
+  yaxis.lab = c(seq(0,12,3), '\u2265 15'),
+  xaxis.cex = 1.5,
+  yaxis.cex = 1.5,
+  xlab.cex = 1.75,
+  ylab.cex = 1.75,
+  xlab.label = '',
+  ylab.label = 'Rank',
+  lwd = 2,
+  key = list(
+    text = list(
+      lab = feature.order,
+      cex = 1,
+      col = 'black'
+    ),
+    lines = list(
+      pch = 19,
+      col = default.colours(12),
+      cex = 1,
+      lwd = 2
+    ),
+    x = 1, 
+    y = 0.75
+  ),
+  right.padding = 23,
+  style = 'Nature'
+)
